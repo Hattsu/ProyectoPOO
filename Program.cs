bc401d613f2c5a891a6a4ebec3385e9a2aa7f566
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-06-07 19:33:06 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
 namespace ProyectoPOO
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Postres postres = new Postres();
-            postres.Panel_opciones_inicio();
+            Usuarios usuarios = new Usuarios("", "", 0, "");
+            usuarios.Panel_usuario();
+            Pasteles pasteles = new Pasteles("", "Pastel", 0, 4, "");
+            pasteles.Panel_opciones_inicio();
+            Cupcake cupcake = new Cupcake("", "Cupcake", 0, 0.25, "");
+            cupcake.Panel_opciones_inicio();
+            Helados helados = new Helados("", "Helado", 0, 0.75, "");
+            helados.Panel_opciones_inicio();
         }
     }
 }
