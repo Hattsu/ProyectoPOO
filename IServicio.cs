10f3a833c6ac6e3280f016fef49af71a78e6501e
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-06-18 09:32:33 -0500

    Update IServicio.cs

--- IServicio.cs
+++ IServicio.cs
@@ -5,8 +5,5 @@ namespace ProyectoPOO
     {
         public void Panel_opciones_inicio();
         public void Cambios_opciones(string Numero_opcion);
-        public void Cancelar_pedido();
-        public void Verificar_pedido();
-        public void Menu_inicio();
     }
 }
